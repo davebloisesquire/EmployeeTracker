0674d202393805b1e3d3db485c11af9dff1541a1
theDaveB <davebloisesquire@gmail.com>
2021-09-19 17:22:02 -0700

    Added new employee, role and dept

--- index.js
+++ index.js
@@ -68,7 +68,63 @@ function viewAllDepartments() {
 }
 
 function addEmployee() {
+  let managerObj;
+  let roleObj;
+  let roleOptions = [];
+  let managerOptions = ["None"];
 
+  db.query('SELECT id, title FROM role', function(err, results) {
+    roleObj = results;
+    for (var i = 0; i < results.length; i++) {
+      roleOptions.push(results[i].title);
+    }
+  });
+
+  db.query('SELECT id, first_name FROM employee WHERE role_id=2', function(err, results) {
+    managerObj = results;
+    for (var i = 0; i < results.length; i++) {
+      managerOptions.push(results[i].first_name);
+    }
+  });
+
+  inq.prompt([{
+      type: 'input',
+      message: 'Employee First Name',
+      name: 'employeeFirstName'
+    },
+    {
+      type: 'input',
+      message: 'Employee Last Name',
+      name: 'employeeLastName'
+    },
+    {
+      type: 'list',
+      message: 'Employee Role',
+      name: 'employeeRole',
+      choices: roleOptions
+    },
+    {
+      type: 'list',
+      message: 'Employee Manager',
+      name: 'employeeManager',
+      choices: managerOptions
+    },
+  ]).then(response => {
+    let managerId = null;
+    const roleId = roleObj.find(({
+      title
+    }) => title === response.employeeRole).id;
+    if (response.employeeManager !== 'None') {
+      managerId = managerObj.find(({
+        first_name
+      }) => first_name === response.employeeManager).id;
+    }
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [response.employeeFirstName, response.employeeLastName, roleId, managerId], function(err, results) {
+      if (err) throw err;
+      console.log("New employee added");
+    });
+    startMenu();
+  })
 };
 
 function updateEmployeeRole() {
@@ -80,15 +136,56 @@ function updateEmployeeManager() {
 };
 
 function addRole() {
+  let deptObj;
+  let deptOptions = [];
 
-};
+  db.query('SELECT id, name FROM department', function(err, results) {
+    deptObj = results;
+    for (var i = 0; i < results.length; i++) {
+      deptOptions.push(results[i].name);
+    }
+  });
 
-function viewAllDepartments() {
-
+  inq.prompt([{
+      type: 'input',
+      message: 'What\'s the new role?',
+      name: 'newRole'
+    },
+    {
+      type: 'input',
+      message: 'What\'s the salary for this new role?',
+      name: 'newSalary'
+    },
+    {
+      type: 'list',
+      message: 'What department is this new role in?',
+      name: 'newRoleDepartment',
+      choices: deptOptions
+    }
+  ]).then(response => {
+    const deptId = deptObj.find(({
+      name
+    }) => name === response.newRoleDepartment).id;
+    db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [response.newRole, response.newSalary, deptId], function(err, results) {
+      if (err) throw err;
+      console.log("New role added");
+    });
+    startMenu();
+  })
 };
 
 function addDepartment() {
-
+  inq.prompt([{
+    type: 'input',
+    message: 'What\'s the new department?',
+    name: 'newDept'
+  }, ]).then(response => {
+    db.query('INSERT INTO department (name) VALUES (?)', [response.newDept], function(err, results) {
+      if (err) throw err;
+      console.log("New department added");
+    });
+    startMenu();
+  })
 };
 
 startMenu();
